cdd4b2fe9d4f8bee770eae7eaa995396fd4a71df
khajidmaa0319 <khajidmaa0319@gmail.com>
2020-05-23 11:15:29 +0900

    Intial Commit

---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export const products = [
+    {
+      name: 'Phone X',
+      price: 769,
+      description: 'The iPhone X was Apple flagship 10th anniversary iPhone featuring a 5.8-inch OLED display, facial recognition and 3D camera functionality, a glass body, and an A11 Bionic processor.',
+      image:"https://img1.kakaku.k-img.com/images/smartphone/icv/f_J0000031198.jpg"
+    },
+    {
+      name: 'Phone 11 ',
+      price: 899,
+      description: 'The iPhone 11 is a smartphone designed, developed, and marketed by Apple Inc. It is the thirteenth generation lower-priced iPhone, succeeding the iPhone XR.',
+      image:"https://img1.kakaku.k-img.com/images/smartphone/icv/f_J0000031198.jpg"
+    },
+    {
+      name: 'Phone 11 PRO',
+      price: 999,
+      description: 'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro.',
+      image:"https://img1.kakaku.k-img.com/images/smartphone/icv/f_J0000031198.jpg"
+    },
+    {
+      name: 'Phone 11 PRO MAX',
+      price: 1099,
+      description: 'The iPhone 11 Pro is available in Gold, Silver, Space Gray, and Midnight Green, a new color previously not available on iPhones. Similar to the iPhone XS, there is a display cutout at the front that includes the 12 MP TrueDepth camera system and speaker. There is also a new rear camera design with three lenses and a flash in a larger, square-shaped bump, which is the most visible difference compared to the iPhone XS.',
+      image:"https://img1.kakaku.k-img.com/images/smartphone/icv/f_J0000031198.jpg"
+    }
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at http://angular.io/license
+  */
